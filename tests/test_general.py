68ec5671a351ddf6327b91afe2a80614cad349f0
Tomas Poveda <tpovedatd@gmail.com>
2020-04-15 17:49:12 +0200

    Now versions are managed by versioneer

--- tests/test_general.py
+++ tests/test_general.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Module that contains general tests for artellapipe-libs-maya
+Module that contains general tests for artellapipe-libs-kitsu
 """
 
 import pytest
@@ -11,4 +11,4 @@ from artellapipe.libs.kitsu import __version__
 
 
 def test_version():
-    assert __version__.__version__
+    assert __version__.get_version()
